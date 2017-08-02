7fc6090bfefa07d77f3553cc6ee8e5c3762dbbd5
Adam Christopher <adam.keristoper@gmail.com>
2017-08-02 23:54:11 +0900

    Game functionallity

--- house.rb
+++ house.rb
@@ -6,6 +6,31 @@ class Room
   attr_accessor :room_name
 end
 
+class Speaker
+  def initialize room
+    @at_room = room
+    @volume = 0
+  end
+
+  attr_accessor :at_room, :volume
+
+  def vup
+    if @volume != 10
+      return @volume += 1
+    else
+      return @volume = 10
+    end
+  end
+
+  def vdown
+    if @volume != 0
+      return @volume -= 1
+    else
+      return @volume = 0
+    end
+  end
+end
+
 class Mom
   def initialize
     @angry = false
@@ -33,5 +58,24 @@ class Mom
   def enter_room room
     @at_room = room
     puts "Mom goes to the #{room.room_name}"
+  end
+end
+
+class House
+  def initialize
+    @mom = Mom.new
+    @lr = Room.new "Living Room"
+    @kitchen = Room.new "Kitchen"
+    @speaker = Speaker.new lr
+  end
+
+  attr_accessor :mom, :lr, :kitchen, :speaker
+
+  def svup speaker
+    speaker.vup
+  end
+
+  def svdown speaker
+    speaker.vdown
   end
 end
